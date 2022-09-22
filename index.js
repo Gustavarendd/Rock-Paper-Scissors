069718548eeaf9fb13c4f32c030067b25d4f3dfe
Gustavarendd <111903425+Gustavarendd@users.noreply.github.com>
2022-09-22 18:03:55 +0200

    made new version
    this works, but need html now

--- index.js
+++ index.js
@@ -1,31 +1,68 @@
-const computerChoice = ['Rock','Paper','Scissors']
-            const choiceNumber = Math.floor(Math.random() * 3);
-            const getComputerChoice = computerChoice[choiceNumber]
-            
-            
-           
-            function playRound(playerSelection, computerSelection) {
-                if (playerSelection == "Rock" && computerSelection == "Paper") {
-                    alert("Player Looses!")
-                } else if (playerSelection == "Scissors" && computerSelection == "Paper") {
-                    alert("Player Wins!")
-                } else if (playerSelection == "Paper" && computerSelection == "Scissors") {
-                alert("Player Looses!")
-                } else if (playerSelection == "Rock" && computerSelection == "Scissors") {
-                    alert("Player Wins!")
-                } else if (playerSelection == "Paper" && computerSelection == "Rock") {
-                    alert("Player Wins!")
-                } else if (playerSelection == "Scissors" && computerSelection == "Rock") {
-                    alert("Player Looses!")
-                } else if (playerSelection != "Rock" || playerSelection != "Paper" || playerSelection != "Scissors") {
-                    alert("Invalid Input!")
-                } else {
-                    alert("It\'s a tie!")
-                }
-        }
-        for (let i = 0; i < 5; i++) {
+const choices = ["rock", "paper", "scissors"];
+let winners = [];
+
+function computerChoice() {
+    return choices[Math.floor(Math.random() * 3)];
+}
+
+function playerChoice() {
+    let input = prompt("Choose: Rock, Paper or Scissors");
+    input = input.toLowerCase();
+    return input;
+}
+
+function playRound(round) {
+    const playerSelection = playerChoice();
+    const computerSelection = computerChoice();
+    const winner = winCheck(playerSelection, computerSelection)
+    winners.push(winner);
+    logRound(playerSelection, computerSelection, winner, round)
+}
+
+function logRound(player, computer, winner, round) {
+    console.log("Round: " + round)
+    console.log("You choose: " + player);
+    console.log("Computer choose: " + computer);
+    console.log(winner);
+    console.log("-------------------------");
+   
+}
+
+
+function winCheck(pSelection, cSelection){
+    if (
+        (pSelection == "rock" && cSelection == "paper") || 
+        (pSelection == "paper" && cSelection == "scissors") || 
+        (pSelection == "scissors" && cSelection == "rock")
+        ) {
+       return "Computer Wins!";
+
+   } else if (pSelection == cSelection) {
+     return "It\'s a tie!";
+
+   } else {
+     return "You Win!";
+   }
+}
+
+function logWins() {
+    let playerWins = winners.filter((item) => item == "You Win!").length;
+    let computerWins = winners.filter((item) => item == "Computer Wins!").length;
+    if (playerWins > computerWins) {
+        return "Congrats! You Win! " + playerWins + " to " + computerWins;
+    } else if (playerWins < computerWins) {
+        return "You Loose! " + playerWins + " to " + computerWins;
+    } else {
+        return "It's a Tie! " + playerWins + " to " + computerWins;
     }
-            const playerSelection = prompt("Choose: Rock, Paper or Scissors");
-            const computerSelection = getComputerChoice;
-            
-            console.log(playRound(playerSelection, computerSelection))
+}
+
+function game() {   
+    for (let i = 1; i <= 5; i++) {
+       playRound(i); 
+    }
+    return "Game Over: " + logWins();
+}
+
+console.log(game())
+
